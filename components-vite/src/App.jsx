278f6fb7f7d5083f06483c2095f05897c18ce520
Piyush <piyushsuthar6300@gmail.com>
2025-11-30 22:38:52 +0530

    component practice

---
+++ components-vite/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Card from './components/Card'
+
+const App = () => {
+
+  const user = ''
+  return (
+    <>
+    <div className='card'>
+      <h1>Piyush Suthar</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet quisquam hic animi minima ipsam quibusdam.</p>
+    </div>
+
+    <Card />
+    <Card />
+    <Card />
+    </>
+  )
+}
+
+export default App
---
+++ components-vite/src/components/Card.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+        <h1 className='cardComp'>This is card component line</h1>
+    </div>
+  )
+}
+ 
+export default Card
