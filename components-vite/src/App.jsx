c36343c4cb2386cc03e3e4c36297b0c2e3609368
Piyush <piyushsuthar6300@gmail.com>
2025-12-01 15:52:14 +0530

    Card component

--- components-vite/src/App.jsx
+++ components-vite/src/App.jsx
@@ -3,17 +3,21 @@ import Card from './components/Card'
 
 const App = () => {
 
-  const user = ''
+  const user = 'Arun Yadav'
+  const age = 21
+
   return (
     <>
     <div className='card'>
-      <h1>Piyush Suthar</h1>
+      <h1>Piyush Suthar & {user}</h1>
+      <h2>Age : {age}</h2> 
       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet quisquam hic animi minima ipsam quibusdam.</p>
     </div>
 
     <Card />
     <Card />
     <Card />
+    {Card()}
     </>
   )
 }
