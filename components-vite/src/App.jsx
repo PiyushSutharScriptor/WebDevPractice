373e691aa327cde3375ca23e155bc3958a300d85
Piyush <piyushsuthar6300@gmail.com>
2025-12-01 16:06:19 +0530

    Components done

--- components-vite/src/App.jsx
+++ components-vite/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Card from './components/Card'
+import Navbar from './components/Navbar'
 
 const App = () => {
 
@@ -8,12 +9,18 @@ const App = () => {
 
   return (
     <>
+    {/* Navbar */}
+    <Navbar />
+
+
+    {/* body */}
     <div className='card'>
       <h1>Piyush Suthar & {user}</h1>
       <h2>Age : {age}</h2> 
       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet quisquam hic animi minima ipsam quibusdam.</p>
     </div>
 
+    {/* Cards */}
     <Card />
     <Card />
     <Card />
