885a05f5b25ab4750687e4294527c3efa458c59e
Piyush <piyushsuthar6300@gmail.com>
2025-12-08 22:53:18 +0530

    tailwind practice

---
+++ tailwind-vite/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-green-600'>
+      <h2 className="text-3xl font-bold underline">
+        Hello world!
+      </h2>
+    </div>
+  )
+}
+
+export default App
---
+++ ui-project-03/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Page1 from './components/page1/page1'
+import Page2 from './components/page2/page2'
+
+const App = () => {
+  return (
+    <div className=''>
+      <Page1 />
+      <Page2 />
+    </div>
+  )
+}
+
+export default App
