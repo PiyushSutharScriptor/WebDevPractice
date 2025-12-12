c0a2262565ea122846bd185b19ee33d3fd5d5080
Piyush <piyushsuthar6300@gmail.com>
2025-12-12 14:03:21 +0530

    Basics event objects

--- function-vite/src/App.jsx
+++ function-vite/src/App.jsx
@@ -1,49 +1,12 @@
 import React from 'react'
+import Functions from './Components/Functions'
+import PageScroll from './Components/PageScroll'
 
 const App = () => {
-
-  const btnClicked = ()=>{
-    console.log("Button clicked")
-  }
-  const mouseEtr = ()=>{
-    alert("Mouse entered")
-  }
-  const pressDown = ()=>{
-    console.log("Key press down")
-  }
-  const inputC = ()=>{
-    console.log("You typed something")
-  }
   return (
-    <div className='hero'>
-      <h1>Hello Piyush</h1>
-      <button className='btn' onClick={btnClicked}>Click Here</button>
-      <button className='btn' onMouseEnter={mouseEtr}>Click Here</button>
-      <button className='btn' onKeyDown={pressDown}>Click Here</button>
-
-      <button className='btn' onClick={()=>{
-        alert("Don't hover on this button")
-      }} >Funtion Button</button>
-
-      <input onChange={()=>{
-        console.log("Input changed")
-      }} className='inpt' type="text" placeholder='Enter Name Here'/>
-
-      <input type="text" className='inpt' onChange={inputC} placeholder='Type Here'/>
-
-      {/* target is used for particular attribute */}
-      <input type="text" className='inpt' onChange={(elem)=>{
-        console.log("Function is called" , elem)
-      }} placeholder='Type to print elem'/>
-
-      <input type="text" className='inpt' onChange={(elem)=>{
-        console.log(elem.target)
-      }} placeholder='Type to print elem.target'/>
-
-      <input type="text" className='inpt' onChange={(elem)=>{
-        console.log(elem.target.value);
-      }} placeholder='Type to print elem.target.value'/>
-
+    <div>
+      <Functions />
+      <PageScroll />
     </div>
   )
 }
