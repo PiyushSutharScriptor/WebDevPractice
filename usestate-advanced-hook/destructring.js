bbc004efeb5850db44d0ca908223906abb292f3b
Piyush <piyushsuthar6300@gmail.com>
2025-12-17 10:21:36 +0530

    destructuring in js

---
+++ usestate-advanced-hook/destructring.js
@@ -0,0 +1,15 @@
+const a = 20
+console.log(a);
+
+let b = {name:'Piyush' , age:20}
+console.log(b);
+
+let c = {...b , city:"Hyderabad"}
+console.log(c);
+
+const {name,age} = b
+console.log(name);
+console.log(b)
+console.log(age);
+
+
