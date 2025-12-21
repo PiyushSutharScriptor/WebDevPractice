5877cb13a27f01760af222335009ed9171bc593d
Piyush <piyushsuthar6300@gmail.com>
2025-12-21 20:04:33 +0530

    mapping the tasks and returning the div's

--- notes-project-05/src/App.jsx
+++ notes-project-05/src/App.jsx
@@ -1,18 +1,28 @@
 import React, { useState } from 'react'
+import { RiDeleteBin5Fill } from "react-icons/ri";
 
 const App = () => {
 
   const[title,setTitle] = useState('');
   const[details,setDetails] = useState('');
+  const[task,setTask] = useState([]);
 
   return (
     <div className='h-full bg-black text-white p-10'>
 
       <form action=""
         onSubmit={(e) => {
-          console.log("Form title : " , {title}) 
-          console.log("Form details : " , {details})
+          // console.log("Form title : " , {title}) 
+          // console.log("Form details : " , {details})
+
           e.preventDefault()
+
+          const copyTask = [...task]
+          copyTask.push({title,details})
+          setTask(copyTask)
+
+          console.log(copyTask);
+          
           setTitle('')
           setDetails('')
         }}
@@ -42,7 +52,7 @@ const App = () => {
             placeholder='Enter Note Details' />
 
           {/* Submit button */}
-          <button className='px-2 py-2 rounded hover:bg-green-500 hover:text-black hover:border-white mb-6 font-medium cursor-pointer border-2 w-fit items-center'>Add Note</button>
+          <button className='px-2 py-2 rounded hover:bg-green-500 hover:text-black hover:border-white mb-6 font-medium cursor-pointer border-2 w-fit items-center active:scale-95'>Add Note</button>
 
         </div>
 
@@ -50,11 +60,17 @@ const App = () => {
         <div className='flex flex-col items-center sm:w-1/3 sm:mx-auto w-full gap-3'>
           <h1 className='font-bold text-3xl'>Recent Notes</h1>
 
-          <div className='w-full flex flex-col gap-3'>
-            <div className='bg-white w-full h-40 rounded'>Card</div>
-            <div className='bg-white w-full h-40 rounded'>Card</div>
-            <div className='bg-white w-full h-40 rounded'>Card</div>
-            <div className='bg-white w-full h-40 rounded'>Card</div>
+            {/* task mapping */}
+          <div className='w-full flex flex-col gap-8'>
+            {task.map((obj,idx)=>{
+              return <div key={idx} className="bg-[url('https://static.vecteezy.com/system/resources/previews/037/152/688/non_2x/sticky-note-paper-background-free-png.png')] relative bg-cover p-4 text-black w-full h-50 rounded overflow-x-hidden overflow-y-hidden">
+                <h1 className='font-bold text-2xl mb-2 mt-5'>{obj.title}</h1>
+                <RiDeleteBin5Fill className='absolute top-10 right-1/10 text-2xl cursor-pointer active:scale-75 text-red-600 ' />
+                <hr />
+                <p className='font-medium mt-2 text-gray-600'>{obj.details}</p>
+              </div>
+            })}
+            {/* <div className='bg-white w-full h-40 rounded'>Card</div> */}
           </div>
 
         </div>
