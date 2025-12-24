9a50fd13e36cbf57e34d89d0c8689a7b876b9c50
Piyush <piyushsuthar6300@gmail.com>
2025-12-24 17:08:20 +0530

    4 methods with obj to str and str to obj

--- local-storage-vite/src/App.jsx
+++ local-storage-vite/src/App.jsx
@@ -2,6 +2,51 @@ import React from 'react'
 
 const App = () => {
 
+  //clear storage : 
+  // localStorage.clear();
+  // sessionStorage.clear();
+
+  //setItem
+  localStorage.setItem("name","Piyush")
+  localStorage.setItem("age",45)
+  localStorage.setItem("city","Hyderabad")
+  
+  //getItem
+  const userName = localStorage.getItem("name");
+  console.log(userName); //Piyush
+  console.log(localStorage.getItem("city")); //Hyderabad
+  
+  //rewrite 
+  localStorage.setItem("name","Umesh")
+  console.log(localStorage.getItem("name")) //Umesh
+
+  //remove
+  localStorage.removeItem("city")
+  console.log(localStorage.getItem("city")) //null
+
+  //object storing (default)
+  const obj = {
+    name : "Toyota",
+    model : "Innova",
+    type : 'Manual'
+  }
+  console.log(obj)
+  localStorage.setItem("vehicleInfo",obj)
+
+  //object storing as String(using stringfy)
+  localStorage.setItem("vehicleString" , JSON.stringify(obj)) 
+
+
+  //printing the JSON.stringify obj
+  const str = localStorage.getItem("vehicleString")
+  console.log(str)
+  console.log(typeof(str))
+
+
+  //converting the JSON.stringify obj into object again(JSON.parse)
+  const convertedObj = JSON.parse(str)
+  console.log(convertedObj)
+  console.log(typeof(convertedObj))
 
   return (
     <div>
