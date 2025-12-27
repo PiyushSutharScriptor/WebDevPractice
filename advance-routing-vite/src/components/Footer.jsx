a49d14b293b5e72c403724776ec3e9d2aeee6b3f
Piyush <piyushsuthar6300@gmail.com>
2025-12-27 23:15:26 +0530

    Basic nested routing (don't use)

--- advance-routing-vite/src/components/Footer.jsx
+++ advance-routing-vite/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='w-full bg-green-700 absolute h-22 flex justify-center items-center bottom-0'>
-        <h2 className='px-4 text-xl font-bold '>Footer</h2>
+        <h2 className='px-4 text-3xl font-bold '>Footer</h2>
     </div>
   )
 }
--- advance-routing-vite/src/pages/Products.jsx
+++ advance-routing-vite/src/pages/Products.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 const Products = () => {
-  return (
-    <h1>Products</h1>
-  )
+    return (
+        <div>
+            <h1>Products</h1>
+            <div className='flex justify-center gap-5 font-semibold text-xl py-4'>
+                <Link  className='border-2 border-green-400 px-3 rounded py-2' to='/products/men'>Men</Link>
+                <Link  className='border-2 border-green-400 px-3 rounded py-2' to='/products/men'>Women</Link>
+            </div>
+        </div>
+    )
 }
 
 export default Products
