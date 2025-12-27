dcb4598b61fbfcd74c6b797de3afcf87bfaec80a
Piyush <piyushsuthar6300@gmail.com>
2025-12-27 18:48:41 +0530

    Basic react routing using BrowserRouter & Link

--- react-router-vite/src/App.jsx
+++ react-router-vite/src/App.jsx
@@ -4,16 +4,20 @@ import Home from './pages/Home'
 import Contact from './pages/Contact'
 import About from './pages/About'
 import Support from './pages/Support'
+import Navbar from './pages/Navbar'
+import Footer from './pages/Footer'
 
 const App = () => {
   return (
     <div>
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='/contact' element={<Contact />}/>
         <Route path='/about' element={<About />}/>
         <Route path='/support' element={<Support />}/>
       </Routes>
+      <Footer />
     </div>
   )
 }
