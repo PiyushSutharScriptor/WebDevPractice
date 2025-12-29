483f56c86978f102a4a271f7895eb5152ee31d23
Piyush <piyushsuthar6300@gmail.com>
2025-12-29 21:54:56 +0530

    useNavigate hook

--- advance-routing-vite/src/pages/About.jsx
+++ advance-routing-vite/src/pages/About.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
-
+import {useNavigate} from 'react-router'
 const About = () => {
+
+  let navigate = useNavigate()
+
+  const btnClick = ()=>{
+    navigate('/')
+  }
   return (
-    <h1>About</h1>
+    <>
+      <h1>About</h1>
+      <button className='px-4 py-3 m-2 font-semibold text-xl rounded cursor-pointer border-2 border-red-400' onClick={btnClick}>Go Home Shortcut</button>
+    </>
   )
 }
 
