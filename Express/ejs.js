6df9d55360c10edada6a36b0de6ac397ffbbf7fb
Piyush <piyushsuthar6300@gmail.com>
2026-01-07 23:05:54 +0530

    Template engines with ejs

---
+++ Express/ejs.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express();
+const port = 3000;
+
+//ejs configuration
+app.set("view engine" , "ejs");
+
+//middleware
+app.use((req,res,next)=>{
+    console.log("Middeware by : " , req.url)
+    next();
+})
+
+//route with ejs file
+app.get('/' , (req,res)=>{
+    res.render("index" , {type : "Piyush Suthar"})
+})
+
+//route with ejs file 2
+app.get('/about' , (req,res)=>{
+    res.render("about" , {age:41})
+})
+
+//port 
+app.listen(port, ()=>{
+    console.log("Server Running");
+}) 
