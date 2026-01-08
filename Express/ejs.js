c1a49618d1c145b3c97d76bb52495b29b11aebc8
Piyush <piyushsuthar6300@gmail.com>
2026-01-08 22:22:06 +0530

    Error handling

--- Express/ejs.js
+++ Express/ejs.js
@@ -24,6 +24,22 @@ app.get('/contact' , (req,res)=>{
     res.render("contact" , {age:41})
 })
 
+//error route
+app.get('/error' , (req,res)=>{
+    throw Error("This is wrong route")
+})
+
+//error handling
+app.use(
+    function errorHandler (err, req, res, next) {
+        if (res.headersSent) {
+            return next(err)
+        }
+        res.status(500)
+        res.render('errorPage' , {error:err}) 
+    }
+)
+
 //port 
 app.listen(port, ()=>{
     console.log("Server Running");
