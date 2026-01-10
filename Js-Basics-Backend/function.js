1564d2c55e7597ac5a104fc26b252e61453df9cf
Piyush <piyushsuthar6300@gmail.com>
2026-01-10 19:47:22 +0530

    Function length (hidden question)

---
+++ Js-Basics-Backend/function.js
@@ -0,0 +1,11 @@
+// we all know length of arr 
+//but what is length of function ?
+
+const fun = (a,b,c)=>{
+
+}
+console.log(fun.length)
+
+//Length of function = number of parameters passed to it.
+//Reason = function are objects in js 
+
