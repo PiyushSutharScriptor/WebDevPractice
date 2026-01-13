bfa25086a1ccf108603f5165a31285522d8a8419
Piyush <piyushsuthar6300@gmail.com>
2026-01-13 22:22:01 +0530

    Update TaskContext.jsx

--- to-do-list-project/src/context/TaskContext.jsx
+++ to-do-list-project/src/context/TaskContext.jsx
@@ -1,14 +1,19 @@
 import { createContext, useEffect, useState } from "react";
 
+//context creator
 export const TaskContext = createContext()
 
+//context provider
 export const TaskProvider = ({ children }) => {
   const [tasks, setTasks] = useState(() => {
+
+    //get the items from localStorage
     let getData = localStorage.getItem("tasks")
     return getData ? JSON.parse(getData) : [];
   });
 
 
+  //update task when new item added
   useEffect(() => {
     localStorage.setItem("tasks", JSON.stringify(tasks))
   }, [tasks])
