5c229a9cfb94e8c056fbdfd6bf46ecf466a8a257
Piyush <piyushsuthar6300@gmail.com>
2026-01-17 23:55:55 +0530

    multiple dynamic routes

--- dynamic-routing-backend/server.js
+++ dynamic-routing-backend/server.js
@@ -6,10 +6,17 @@ app.get('/' ,(req,res)=>{
     res.send("Main Page")
 })
 
+//dynamic routes
 app.get('/profile/:id' , (req,res)=>{
-    res.send("Dynamic Route")
+    res.send(`Dynamic Route for ${req.params.id}`)
     console.log(req.url)
     console.log(req.params.id)
+})
+
+app.get('/profile/:user/:age' , (req,res)=>{
+    res.send(`Dynamic Route for ${req.params.user} of age : ${req.params.age}`)
+    console.log(req.url)
+    console.log(req.params.age)
 })
 
 app.listen(port , ()=>{
