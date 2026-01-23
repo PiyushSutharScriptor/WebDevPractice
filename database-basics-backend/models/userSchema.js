7e124f00f31394877711dfa24b09e0a5b138e18a
Piyush <piyushsuthar6300@gmail.com>
2026-01-23 21:13:27 +0530

    schema and model creation

---
+++ database-basics-backend/models/userSchema.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+        name:{
+            type:String,
+            required:true,
+            unique:true
+        },
+        age:{
+            type:Number,
+            required:true,
+            unique:true
+        },
+
+    },
+    
+    {timestamp:true}
+)
+
+const user = mongoose.model("user" , userSchema)
+
+module.exports = user;
