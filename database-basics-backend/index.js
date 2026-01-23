f5e186d37a003218f06e36acff082796a1e2c8d8
Piyush <piyushsuthar6300@gmail.com>
2026-01-23 19:16:34 +0530

    mongoDB atlas connection with express

--- database-basics-backend/index.js
+++ database-basics-backend/index.js
@@ -1,11 +1,19 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const connectDb = require('./config/db')
+
 const app = express();
-const port = 3000;
+
+//logad environment variables from .env file
+dotenv.config()
+//mongoDB function execution
+connectDb()
 
 app.get('/' ,(req,res)=>{
     res.send("Home Page")
 })
 
-app.listen(port , ()=>{
+app.listen(process.env.PORT , ()=>{
     console.log("Server running...")
 })
