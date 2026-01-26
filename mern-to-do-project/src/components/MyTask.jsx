afd3b5b49a43881566e8edd3189a4cc8224b2cfc
Piyush <piyushsuthar6300@gmail.com>
2026-01-26 23:34:04 +0530

    My task component react

---
+++ mern-to-do-project/src/components/MyTask.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { HiClipboardDocumentList } from "react-icons/hi2";
+
+
+const MyTask = () => {
+  return (
+    <>
+    <div className='text-white flex items-center gap-2 text-2xl'>
+      <h1 className='text-gray-600 font-medium'>My Tasks </h1>
+      <span className='text-green-400'><HiClipboardDocumentList /></span>
+    </div>
+    <div>
+      <div>
+
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default MyTask
