125ee4daf4a4493d7cd551255edb7dc727c87417
Piyush <piyushsuthar6300@gmail.com>
2026-01-27 11:55:30 +0530

    Task buttons and transition

--- mern-to-do-project/src/components/AddTask.jsx
+++ mern-to-do-project/src/components/AddTask.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { MdOutlinePostAdd } from "react-icons/md";
+import { AiOutlineClear } from "react-icons/ai";
+
 
 const AddTask = () => {
   return (
@@ -13,9 +15,16 @@ const AddTask = () => {
         <textarea rows={4} className='bg-zinc-900 resize-none border-2 border-green-300 rounded-md w-full mt-4 p-3 ' placeholder='Enter Details' ></textarea>
  
         {/* button */}
-        <button className='bg-green-800 transition font-medium px-3 gap-2 py-2 flex mt-2 rounded-md cursor-pointer border-white border-2  hover:scale-90 items-center'>Add Task 
-          <h1 className='pb-0.5 text-xl'><MdOutlinePostAdd /></h1>
-        </button>
+        <div className='flex gap-3 '>
+
+          <button className='bg-green-800 transition font-medium px-3 gap-2 py-2 flex mt-2 rounded-md cursor-pointer border-white border-2  hover:scale-90 items-center'>Add Task 
+            <h1 className='pb-0.5 text-xl'><MdOutlinePostAdd /></h1>
+          </button>
+
+          <button className='bg-yellow-500 transition font-medium px-3 gap-2 py-2 flex mt-2 rounded-md cursor-pointer text-black border-white border-2  hover:scale-90 items-center'>Clear Fields
+            <h1 className='pb-0.5 text-xl'><AiOutlineClear /></h1>
+          </button>
+        </div>
       </form>
     </div>
   )
--- mern-to-do-project/src/components/MyTask.jsx
+++ mern-to-do-project/src/components/MyTask.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { HiClipboardDocumentList } from "react-icons/hi2";
+import { FaEdit } from "react-icons/fa";
+import { RiDeleteBin6Fill } from "react-icons/ri";
 
 
 const MyTask = () => {
@@ -16,7 +18,20 @@ const MyTask = () => {
       <div className='sm:w-72 w-full border-2 hover:scale-105 transition border-green-600 p-2 rounded bg-zinc-900'>
         <h1 className='text-white mb-2 font-semibold text-2xl'>Title</h1>
         <p className='text-zinc-500'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis hic qui veritatis, repellat maiores sed esse voluptatibus aliquid impedit corporis!</p>
+        
+        <div className='flex items-center justify-center gap-2 mt-2 w-full'>
+
+          <button className='bg-yellow-500 transition gap-2 p-2 flex mt-3 rounded-md cursor-pointer text-black border-white border-2  hover:scale-90 items-center text-sm'>
+          <h1 className='text-xl'><FaEdit /></h1>
+          </button>
+
+          <button className='bg-red-500 transition gap-2 p-2 flex mt-3 rounded-md cursor-pointer text-white border-white border-2  hover:scale-90 items-center text-sm'>
+          <h1 className='text-xl'><RiDeleteBin6Fill /></h1>
+          </button>
+
+        </div>
       </div>
+      
       
 
     </div>
