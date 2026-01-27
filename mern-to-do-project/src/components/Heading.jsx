07ba0b2d8f3c3c33f09b07e27c59ebbdc31ea008
Piyush <piyushsuthar6300@gmail.com>
2026-01-27 11:55:52 +0530

    Logout button and responsiveness

--- mern-to-do-project/src/components/Heading.jsx
+++ mern-to-do-project/src/components/Heading.jsx
@@ -1,17 +1,27 @@
 import React from 'react'
 import { SiHey } from "react-icons/si";
 import { FaRegHandPointDown } from "react-icons/fa";
+import { FaSignOutAlt } from "react-icons/fa";
 
 
 const Heading = () => {
   return (
     <div className='pt-8 '>
-        <div className='flex items-center text-white text-3xl font-bold sm:text-5xl gap-4'>
-          <h1 className=''>Hello<span className='text-green-400'>,</span> Payush</h1>
-          <span className='text-green-400 transition hover:animate-[shake_0.3s_ease-in-out_infinite]'><SiHey /></span>
+        <div className='flex items-center justify-between text-white text-3xl font-bold sm:text-5xl'>
+
+          <div className='flex gap-4'>
+            <h1 className=''>Hello<span className='text-green-400'>,</span> Payush</h1>
+            <span className='text-green-400 transition animate-[shake_0.5s_ease-in-out_infinite]'><SiHey /></span>
+          </div>
+
+          <div className='flex text-sm font-medium cursor-pointer hover:scale-110 transition border-white border-2 px-2 gap-2 py-1 rounded-md items-center bg-red-600'>
+            <h1>Give Up</h1>
+            <h1><FaSignOutAlt /></h1>
+          </div>
+
         </div>
 
-        <div className='text-gray-600 font-medium text-2xl mt-10 flex items-center  gap-3'>
+        <div className='text-gray-600 font-medium text-2xl mt-10 flex items-center gap-3'>
           <h1 className=''>Add Task </h1>
           <span className='text-green-600'><FaRegHandPointDown /></span>
         </div>
