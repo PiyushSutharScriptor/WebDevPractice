13a46556b396ce98227a80790bbb7bf8cc34e527
Piyush <piyushsuthar6300@gmail.com>
2026-01-28 23:48:49 +0530

    Routes Route and BrowerRoues

--- mern-to-do-project/src/App.jsx
+++ mern-to-do-project/src/App.jsx
@@ -3,15 +3,32 @@ import Heading from './components/Heading'
 import AddTask from './components/AddTask'
 import MyTask from './components/MyTask'
 import Line from './components/Line'
+import LoginPage from './components/LoginPage'
+import { Routes, Route } from 'react-router-dom'
+import SignUpPage from './components/SignUpPage'
 
 const App = () => {
   return (
-    <div className='bg-black h-full px-10 py-6'>
-      <Heading />
-      <AddTask />
-      <Line />
-      <MyTask />
-    </div>
+    <>
+      <Routes>
+
+        <Route
+          path='/' element={
+            <div className='bg-black h-full px-10 py-6'>
+              <Heading />
+              <AddTask />
+              <Line />
+              <MyTask />
+            </div>
+          }
+        />
+
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/signup' element={<SignUpPage />} />
+
+      </Routes>
+    </>
+
   )
 }
 
--- mern-to-do-project/src/components/Heading.jsx
+++ mern-to-do-project/src/components/Heading.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { SiHey } from "react-icons/si";
 import { FaRegHandPointDown } from "react-icons/fa";
 import { FaSignOutAlt } from "react-icons/fa";
-
+import { Link } from 'react-router-dom';
 
 const Heading = () => {
   return (
@@ -14,13 +14,15 @@ const Heading = () => {
             <span className='text-green-400 transition animate-[shake_0.5s_ease-in-out_infinite]'><SiHey /></span>
           </div>
 
-          <div className='flex text-sm font-medium cursor-pointer hover:scale-110 transition border-white border-2 px-2 gap-2 py-1 rounded-md items-center bg-red-600'>
+          {/* logout */}
+          <Link to='/login' className='flex text-sm font-medium cursor-pointer hover:scale-110 transition border-white border-2 px-2 gap-2 py-1 rounded-md items-center bg-red-600'>
             <h1>Give Up</h1>
             <h1><FaSignOutAlt /></h1>
-          </div>
+          </Link  >
 
         </div>
 
+        {/* Add task heading */}
         <div className='text-gray-600 font-medium text-2xl mt-10 flex items-center gap-3'>
           <h1 className=''>Add Task </h1>
           <span className='text-green-600'><FaRegHandPointDown /></span>
