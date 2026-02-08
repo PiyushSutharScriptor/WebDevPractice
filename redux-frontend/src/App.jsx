abf05bb9ae8e221f9de01daf6db4b98493a57ffa
Piyush <piyushsuthar6300@gmail.com>
2026-02-08 16:22:20 +0530

    redux toolkit basics

--- redux-frontend/src/App.jsx
+++ redux-frontend/src/App.jsx
@@ -1,13 +1,29 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { decrement, increment } from './redux/slices/counterSlice'
 
 const App = () => {
+
+  const dispatch = useDispatch()
+  const count = useSelector((state)=>state.counter.value)
+
   return (
     <div>
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <h1>{count}</h1>
+
+      <button className='' onClick={()=>{
+        dispatch(increment())
+      }}>
+        Increment
+      </button>
+
+      <button className='' onClick={()=>{
+        dispatch(decrement())
+      }}>
+        Increment
+      </button>
     </div>
   )
-} 
+}
 
 export default App
