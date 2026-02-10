05eda20a890e354e24f35fa1f8afec2b6fee5ab0
Piyush <piyushsuthar6300@gmail.com>
2026-02-10 22:09:25 +0530

    redux project initialization and setup

---
+++ redux-project/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <div className='bg-zinc-800 w-full h-screen text-white'>
+      <h1>Piyush Suthar</h1>  
+    </div>
+    </>
+  )
+}
+
+export default App
