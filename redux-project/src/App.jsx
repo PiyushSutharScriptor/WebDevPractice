dc2973ee6780bb9c76b62d470a400a0441fc0564
Piyush <piyushsuthar6300@gmail.com>
2026-02-11 22:44:39 +0530

    calling pexels and unsplash api for photo and video

--- redux-project/src/App.jsx
+++ redux-project/src/App.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
-import { getData } from './api/mediaApi'
+import { getData, getVideo } from './api/mediaApi'
 
 const App = () => {
 
   return (
     <>
     <div className='bg-zinc-800 w-full h-screen text-white'>
+
       <h1>Piyush Suthar</h1>  
-      <button onClick={()=>{
-        getData('cat')
+
+      <button onClick={async ()=>{
+        const data = await getData('cat')
+        console.log(data)
       }}>Get Photos</button>
+
+      <button onClick={async ()=>{
+        const data = await getVideo('cat')
+        console.log(data)
+      }}>Get Video</button>
+
     </div>
     </>
   )
--- redux-project/src/api/mediaApi.js
+++ redux-project/src/api/mediaApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios'   
 
-const U_KEY = import.meta.env.UNSPLASH_KEY
-const P_KEY = import.meta.env.PEXELS_KEY
+const U_KEY = import.meta.env.VITE_UNSPLASH_KEY
+const P_KEY = import.meta.env.VITE_PEXELS_KEY
 
 export const getData = async(query,page=1,per_page=15)=>{
     try{
@@ -9,9 +9,29 @@ export const getData = async(query,page=1,per_page=15)=>{
             params:{query,page,per_page},
             headers:{Authorization:`Client-ID ${U_KEY}`}
         })
-        console.log(res)
+        // console.log(res)
+        // return res.data
+        return res.data.results
     }
     catch(err){
         console.log("Error : " , err);
+    }
+}
+
+
+export const getVideo = async(query,per_page=15)=>{
+    try{
+        const res = await axios.get('https://api.pexels.com/videos/search',{
+            params:{query,per_page},
+            headers:{Authorization:P_KEY}
+        })
+
+        // return res
+        // return res.data
+        return res.data.videos
+
+    }
+    catch(err){
+        console.log("Error : " , err)
     }
 }
