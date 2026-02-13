08f28aaecbd427b560dd7f602a21861a54105a8e
Piyush <piyushsuthar6300@gmail.com>
2026-02-13 21:59:05 +0530

    from handling and two way binding

--- redux-project/src/components/SearchBar.jsx
+++ redux-project/src/components/SearchBar.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FaSearchengin } from "react-icons/fa";
 
 const SearchBar = () => {
+
+  const [text, setText] = useState("")
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    // console.log("From Submitted")
+    setText("")
+  }
+
   return (
     <div className='bg-zinc-700 py-3'>
-      <form className='flex gap-3 w-full items-center justify-around'>
+      <form onSubmit={(e)=>{
+        submitHandler(e)
+      }} className='flex gap-3 w-full items-center justify-around'>
 
         <div>
           {/* heading */}
@@ -14,6 +25,12 @@ const SearchBar = () => {
         <div className="flex items-center border-2 border-white rounded-full overflow-hidden">
           {/* search bar */}
           <input
+            required
+            value={text}
+            onChange={(e)=>{
+              // console.log(e.target.value)
+              setText(e.target.value)
+            }}
             className="w-64 px-4 h-12 bg-transparent text-white placeholder-gray-300 outline-none"
             type="text"
             placeholder="search anything..."
