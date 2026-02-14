4727440ea86b6436e5597f0b9f4d13eefa32da69
Piyush <piyushsuthar6300@gmail.com>
2026-02-14 23:38:29 +0530

    changes

--- redux-project/src/components/Tabs.jsx
+++ redux-project/src/components/Tabs.jsx
@@ -8,7 +8,7 @@ const Tabs = () => {
     <div className='flex gap-8 items-center w-full justify-center mt-4'>
 
       {btn.map((ele,idx)=>{
-        return <button className='border-2 border-zinc-900 bg-zinc-500 px-2 py-1 rounded text-white font-semibold text-xl hover:bg-blue-700 cursor-pointer hover:scale-98 transition-all' key={idx} >{ele}</button>
+        return <button className='border-2 border-zinc-900 bg-zinc-500 px-4 py-2 rounded text-white font-semibold text-xl hover:bg-blue-700 cursor-pointer hover:scale-98 transition-all uppercase' key={idx} >{ele}</button>
       })}
 
     </div>
