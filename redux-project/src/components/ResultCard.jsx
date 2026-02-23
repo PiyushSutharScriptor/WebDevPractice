b3a8a2d2f45c7322c027f6e989cdefd111197c78
Piyush <piyushsuthar6300@gmail.com>
2026-02-23 23:26:14 +0530

    result card component

--- redux-project/src/components/ResultCard.jsx
+++ redux-project/src/components/ResultCard.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 
-const ResultCard = () => {
+const ResultCard = (props) => {
   return (
-    <div>ResultCard</div>
+    <div className='flex mx-auto'>
+      {
+        props.item.type==='photo' ?
+        (<img src={props.item.src} className='object-cover rounded-xl w-60 h-60' alt="image" />) : 
+        (<video src={props.item.src} controls/>)
+      }
+
+    </div>
   )
 }
 
