e98f1a47d4354c8bead35e4e5195990201ca8d58
Piyush <piyushsuthar6300@gmail.com>
2026-02-25 22:37:26 +0530

    GridCard styling and naming of items

--- redux-project/src/components/ResultCard.jsx
+++ redux-project/src/components/ResultCard.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 const ResultCard = (props) => {
   return (
-    <div className='flex'>
+    <div className='flex relative'>
       {
         props.item.type==='photo' ?
         (<img src={props.item.src} className='object-cover rounded-xl w-60 h-60' alt="image" />) : 
-        (<video src={props.item.src} className='object-cover rounded-xl w-60 h-60' controls/>)
+        (<video src={props.item.src} autoPlay loop muted className='object-cover rounded-xl w-60 h-60'/>)
       }
-
+      <div className='w-full absolute capitalize bg-linear-to-b from-transparent to-black text-white p-2 font-semibold bottom-0'>{props.item.title}</div>
     </div>
   )
 }
