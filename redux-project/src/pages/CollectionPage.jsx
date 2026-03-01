1439635137206da9a707c57dcb7a4e60244df7d0
Piyush <piyushsuthar6300@gmail.com>
2026-03-02 00:07:21 +0530

    Collection page mapping with localstorage

--- redux-project/src/pages/CollectionPage.jsx
+++ redux-project/src/pages/CollectionPage.jsx
@@ -1,23 +1,24 @@
 import React from 'react'
-import ResultCard from '../components/ResultCard';
+import ResultCard from '../components/ResultCard'
 
 const CollectionPage = () => {
-
-  const data = JSON.parse(localStorage.getItem(('collection')));
-  console.log(data)
+  const data = JSON.parse(localStorage.getItem('collection')) || []
+  console.log(data.length)
 
   return (
-    <div>
-      {
-        data.map((item)=>{
-          return <>
-          <div>
-            <ResultCard item={item} />
+    <>
+      {data.length > 1 ? (
+        <div className="w-full max-w-8xl mx-auto px-6">
+          <div className="flex flex-wrap gap-4 my-8">
+            {data.map((item, idx) => (
+              <ResultCard key={idx} idx={idx} item={item} />
+            ))}
           </div>
-          </>
-        })
-      }
-    </div>
+        </div>
+      ) : (
+        <h1>sdf</h1>
+      )}
+    </>
   )
 }
 
