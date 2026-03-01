7063d6e6bb6ed6ba9ba0148ba38a563ead30956c
Piyush <piyushsuthar6300@gmail.com>
2026-03-01 22:58:45 +0530

    storing collection in local storage

--- redux-project/src/components/ResultCard.jsx
+++ redux-project/src/components/ResultCard.jsx
@@ -1,6 +1,19 @@
 import React from 'react'
 
 const ResultCard = (props) => {
+
+  const addToCollection = (item)=>{
+    // console.log("Button clicked" , item)
+
+    let oldData = JSON.parse(localStorage.getItem('collection')) || [];
+    // console.log(oldData)
+
+    let newData = [...oldData, item];
+    console.log(newData)
+
+    localStorage.setItem('collection',JSON.stringify(newData))
+
+  }
   return (
     
     <div className='flex relative' key={props.idx}>
@@ -8,14 +21,16 @@ const ResultCard = (props) => {
       <a target='_blank' href={props.item.url}>
       {
         props.item.type==='photo' ?
-        (<img src={props.item.src} className='object-cover rounded-xl w-60 h-60' alt="image" />) : 
+        (<img src={props.item.src} className='object-cover rounded-xl sm:w-60 w-96 h-60' alt="image" />) : 
         (<video src={props.item.src} autoPlay loop muted className='object-cover rounded-xl w-60 h-60'/>)
       }
       </a>
 
       <div className='w-full flex justify-center gap-2 items-center absolute capitalize bg-linear-to-b from-transparent to-black text-white p-2 bottom-0'>
         <div className='font-medium h-14 overflow-hidden'>{props.item.title}</div>
-        <button className='cursor-pointer rounded px-2 py-1 bg-blue-500 text-white text-sm font-semibold hover:bg-blue-700 active:scale-95'>Save</button>
+        <button onClick={()=>{
+          addToCollection(props.item)
+        }} className='cursor-pointer rounded px-2 py-1 bg-blue-500 text-white text-sm font-semibold hover:bg-blue-700 active:scale-95'>Save</button>
       </div>
 
     </div>
